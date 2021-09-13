14e5f1dcf4691d67e95196c69e0fce8b6c71da57
Aliaksei Paliakou <alpa1218@netcracker.com>
2021-09-14 00:56:13 +0300

    [hw04-gc] - fixed table of results

--- hw04-gc/src/main/java/ru/otus/hw04/CalcDemo.java
+++ hw04-gc/src/main/java/ru/otus/hw04/CalcDemo.java
@@ -13,15 +13,17 @@ package ru.otus.hw04;
 /*
 ----------------------------------------- Results (without optimization): -------------------------------------------
 -Xmx256m:                 |  -Xmx2048m:                |  -Xmx4096m:                |  -Xmx1900m:
-spend msec:45482, sec:45  |  spend msec:21232, sec:21  |  spend msec:22613, sec:22  |  spend msec:20792, sec:20
-spend msec:42244, sec:42  |  spend msec:21376, sec:21  |  spend msec:21808, sec:21  |  spend msec:20331, sec:20
-spend msec:43268, sec:43  |  spend msec:21094, sec:21  |  spend msec:22316, sec:22  |  spend msec:20702, sec:20
+spend msec:31647, sec:31  |  spend msec:15158, sec:15  |  spend msec:13718, sec:13  |  spend msec:14520, sec:14
+spend msec:31666, sec:31  |  spend msec:13839, sec:13  |  spend msec:14719, sec:14  |  spend msec:13527, sec:13
+spend msec:31914, sec:31  |  spend msec:14504, sec:14  |  spend msec:14899, sec:14  |  spend msec:15159, sec:15
 -Xmx1500m:                |  -Xmx1600m:                |  -Xmx1800m:                |  -Xmx1850m:
-spend msec:22243, sec:22  |  spend msec:21718, sec:21  |  spend msec:21857, sec:21  |  spend msec:19772, sec:19
-spend msec:22678, sec:22  |  spend msec:21923, sec:21  |  spend msec:22074, sec:22  |  spend msec:19770, sec:19
-spend msec:22567, sec:22  |  spend msec:22145, sec:22  |  spend msec:21784, sec:21  |  spend msec:19811, sec:19
-                                                                                    |  spend msec:19533, sec:19
-                                                                                    |  spend msec:19415, sec:19
+spend msec:15430, sec:15  |  spend msec:15365, sec:15  |  spend msec:14497, sec:14  |  spend msec:12175, sec:12
+spend msec:16087, sec:16  |  spend msec:15709, sec:15  |  spend msec:15502, sec:15  |  spend msec:11855, sec:11
+spend msec:16120, sec:16  |  spend msec:15693, sec:15  |  spend msec:15031, sec:15  |  spend msec:13062, sec:13
+-Xmx2148m:                | -Xmx2248m                  | -Xmx2448m
+spend msec:12334, sec:12  |  spend msec:14962, sec:14  |  spend msec:14858, sec:14
+spend msec:14971, sec:14  |  spend msec:15050, sec:15  |  spend msec:15702, sec:15
+spend msec:14991, sec:14  |  spend msec:14688, sec:14  |  spend msec:14916, sec:14
 ------------------------------------------ Results (with int optimization): -----------------------------------------
 -Xmx256m:                 |  -Xmx1850m:              |  -Xmx500m:
 spend msec:6582, sec:6    |  spend msec:4512, sec:4  |  spend msec:4152, sec:4
