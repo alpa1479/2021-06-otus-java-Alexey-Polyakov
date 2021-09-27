3a6dc193616fb33d5c3e4078ae8d3edc7b9c32df
Aliaksei Paliakou <alpa1218@netcracker.com>
2021-09-27 17:32:19 +0300

    [hw07-structuralPatterns] - Replaced Mockito mock to Mock implementation of DateTimeProvider interface

--- hw07-structuralPatterns/src/test/java/ru/otus/hw07/handler/EvenSecondExceptionProcessorTest.java
+++ hw07-structuralPatterns/src/test/java/ru/otus/hw07/handler/EvenSecondExceptionProcessorTest.java
@@ -3,17 +3,13 @@ package ru.otus.hw07.handler;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import ru.otus.hw07.handler.mocks.MockDateTimeProviderImpl;
 import ru.otus.hw07.model.Message;
 import ru.otus.hw07.processor.Processor;
-import ru.otus.hw07.processor.homework.DateTimeProvider;
 import ru.otus.hw07.processor.homework.EvenSecondException;
 import ru.otus.hw07.processor.homework.EvenSecondExceptionProcessor;
 
-import java.time.LocalDateTime;
-
 import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class EvenSecondExceptionProcessorTest {
 
@@ -22,10 +18,7 @@ public class EvenSecondExceptionProcessorTest {
     void shouldThrowExceptionInEvenSecond() {
         // given
         final Message emptyMessage = new Message.Builder(1).build();
-        final DateTimeProvider dateTimeProvider = mock(DateTimeProvider.class);
-        final LocalDateTime constantTime = LocalDateTime.of(2021, 1, 1, 1, 1, 2);
-        when(dateTimeProvider.getDate()).thenReturn(constantTime);
-        final Processor processor = new EvenSecondExceptionProcessor(dateTimeProvider);
+        final Processor processor = new EvenSecondExceptionProcessor(new MockDateTimeProviderImpl());
 
         // when
         final Throwable thrown = catchThrowable(() -> processor.process(emptyMessage));
